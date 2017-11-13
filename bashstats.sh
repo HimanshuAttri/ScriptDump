00543c1188758755765d2933f82af32d41f64eea
Himanshu Attri <attri.him@gmail.com>
2017-11-14 04:59:03 +0530

    bash ranker added

---
+++ bashstats.sh
@@ -0,0 +1,4 @@
+# command to list and rank top 10 commands used by user with counts
+history | awk '{CMD[$2]++;count++;}END { for (a in CMD)print CMD[a] " " CMD[a]/count*100 "% " a;}' | grep -v "./" |
+column -c3 -s " " -t | sort -nr | nl |  head -n10
+
